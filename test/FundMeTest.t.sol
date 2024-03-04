87938850eb388ac12d9de2cc3e70032aaf0a987b
Anil <abasdeo29@gmail.com>
2024-03-04 18:09:59 -0500

    added tests and broke app -> abandon project

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -2,16 +2,25 @@
 pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
-import {FundMe} from "../src/FundMe.sol"; 
+import {FundMe} from "../src/FundMe.sol";
 
 contract FundMeTest is Test {
-  FundMe fundMe;
+    FundMe fundMe;
 
-  function setUp() external {
-    fundMe = new FundMe();
-  }
+    function setUp() external {
+        fundMe = new FundMe();
+    }
 
-  function testMinimumDollarIsFive() public {
-    assertEq(fundMe.MINIMUM_USD(), 5e18);
-  }
+    function testMinimumDollarIsFive() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMessageSender() public {
+        assertEq(fundMe.i_owner(), address(this));
+    }
+
+    function testPriceFeedVersionIsAccurate() public {
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
+    }
 }
